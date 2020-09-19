9f90ad871fea07fb5771634615cefb9584762b32
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2020-09-19 16:05:04 -0300

    emerson-soares

---
+++ Python/The Varieties/variáveis-primitivas-trybe.py
@@ -0,0 +1,31 @@
+#-------------------------------------
+# CURSO BÁSICO DE PYTHON | AULA 01 |
+#   Variáveis Primitivas (Trybe)
+#------------------------------------
+
+nome = 'Emerson Soares da Silva'
+idade = 19
+altura = 1.75
+tutor = False
+trabalhando = True
+estudando = True
+
+print(nome)
+print(idade)
+print(altura)
+print(tutor)
+print(trabalhando)
+
+nova_idade = idade + 30
+nome_cidade = nome + ' São Paulo'
+
+print(nova_idade)
+print(nome_cidade)
+
+altura_sem_tenis = altura - 5.0
+
+print(altura_sem_tenis)
+
+resultado = idade + altura
+
+print(resultado)
