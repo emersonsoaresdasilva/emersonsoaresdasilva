2e2e12ac6c8d3237fa2b8eb5956071696cde54e6
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2020-11-28 16:51:47 -0300

    emerson-soares

---
+++ Python/FIAP - Turn the future ON!/Cápitulo 5 - Manipulação de Arquivos/manipular-json.py
@@ -0,0 +1,13 @@
+#Um caminho para portabilidade.
+from funcoes_json import *
+
+inventario = ler_arquivo("inventario_json.json")
+
+opcao = chamarMenu()
+
+while opcao > 0 and opcao < 3:
+    if opcao == 1:
+        print(registrar(inventario, "inventario_json.json"))
+    elif opcao == 2:
+        exibir("inventario_json.json")
+    opcao = chamarMenu()
