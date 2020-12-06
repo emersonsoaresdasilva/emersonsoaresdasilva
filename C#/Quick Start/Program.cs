b48db05b5adde3091d6c4393fb6c5179ec9690c1
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2020-12-05 22:37:47 -0300

    emerson-soares

---
+++ C#/Quick Start/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(" ------------------------------------------------");
+		Console.WriteLine("|                                                |");
+		Console.WriteLine("| Seja bem-vindo ao meu primeiro programa em C#. |");
+		Console.WriteLine("|                                                |");
+		Console.WriteLine(" ------------------------------------------------");
+		string username = null;
+		string password = null;
+
+		DateTime timeUtc = DateTime.UtcNow;
+		TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+		DateTime dateTimeBrasilia = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, kstZone);
+
+		do
+		{
+			Console.Write("\nDigite o seu usuário: ");
+			username = Console.ReadLine();
+			Console.Write("Digite a sua senha: ");
+			password = Console.ReadLine();
+
+			if (username != "Emerson" || password != "!is@C#")
+				Console.Clear();
+				Console.WriteLine("\nUsuário ou senha incorretos, tente novamente!");
+		}
+		while (username != "Emerson" || password != "!is@C#");
+			Console.Clear();
+			Console.WriteLine("\nLogin efetuado com sucesso!");
+			Console.WriteLine("\nAcessado em: " + dateTimeBrasilia.ToString("dd/MM/yyyy HH:mm:ss"));
+			Console.ReadKey();
+	}
+}
+
