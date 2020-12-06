336886c3c939ecb386739f80a05153820f1b1dfe
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2020-12-05 22:50:30 -0300

    emerson-soares

--- C#/Quick Start/Program.cs
+++ C#/Quick Start/Program.cs
@@ -9,29 +9,27 @@ public class Program
 		Console.WriteLine("| Seja bem-vindo ao meu primeiro programa em C#. |");
 		Console.WriteLine("|                                                |");
 		Console.WriteLine(" ------------------------------------------------");
-		string username = null;
-		string password = null;
-
-		DateTime timeUtc = DateTime.UtcNow;
-		TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+        DateTime timeUtc = DateTime.UtcNow;
+        TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
 		DateTime dateTimeBrasilia = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, kstZone);
 
-		do
-		{
-			Console.Write("\nDigite o seu usuário: ");
-			username = Console.ReadLine();
-			Console.Write("Digite a sua senha: ");
-			password = Console.ReadLine();
+        string password;
+        string username;
+        do
+        {
+            Console.Write("\nDigite o seu usuário: ");
+            username = Console.ReadLine();
+            Console.Write("Digite a sua senha: ");
+            password = Console.ReadLine();
 
-			if (username != "Emerson" || password != "!is@C#")
-				Console.Clear();
-				Console.WriteLine("\nUsuário ou senha incorretos, tente novamente!");
-		}
-		while (username != "Emerson" || password != "!is@C#");
-			Console.Clear();
-			Console.WriteLine("\nLogin efetuado com sucesso!");
-			Console.WriteLine("\nAcessado em: " + dateTimeBrasilia.ToString("dd/MM/yyyy HH:mm:ss"));
-			Console.ReadKey();
+            if (username != "Emerson" || password != "!is@C#")
+                Console.Clear();
+                Console.WriteLine("\nUsuário ou senha incorretos, tente novamente!");
+        }
+        while (username != "Emerson" || password != "!is@C#");
+            Console.Clear();
+		    Console.WriteLine("\nLogin efetuado com sucesso!");
+		    Console.WriteLine("\nAcessado em: " + dateTimeBrasilia.ToString("dd/MM/yyyy HH:mm:ss"));
+		    Console.ReadKey();
 	}
 }
-
