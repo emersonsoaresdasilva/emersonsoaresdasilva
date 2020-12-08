cf9cb8c0f58e17da4a23c6a390878e543d4015cb
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2020-12-07 21:28:29 -0300

    emerson-soares

--- C#/Quick Start/Program.cs
+++ C#/Quick Start/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 
 public class Program
 {
@@ -9,12 +14,18 @@ public class Program
 		Console.WriteLine("| Seja bem-vindo ao meu primeiro programa em C#. |");
 		Console.WriteLine("|                                                |");
 		Console.WriteLine(" ------------------------------------------------");
-        DateTime timeUtc = DateTime.UtcNow;
         TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+        DateTime timeUtc = DateTime.UtcNow;
 		DateTime dateTimeBrasilia = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, kstZone);
 
         string password;
         string username;
+        bool admin = false;
+        //List<string> logs = new List<string>();
+        //foreach (var log in logs){
+        //    Console.WriteLine($"Último acesso: {logs}");
+        //}
+
         do
         {
             Console.Write("\nDigite o seu usuário: ");
@@ -22,14 +33,20 @@ public class Program
             Console.Write("Digite a sua senha: ");
             password = Console.ReadLine();
 
-            if (username != "Emerson" || password != "!is@C#")
+            if (username != "Emerson" || password != "!is@C#") 
+            {
                 Console.Clear();
                 Console.WriteLine("\nUsuário ou senha incorretos, tente novamente!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"{username} logou com sucesso!", admin = true);
+                Console.WriteLine($"\nAcessado em: {dateTimeBrasilia.ToString("dd/MM/yyyy HH:mm:ss")} - Horário em Brasília - DF");
+                Console.WriteLine($"\nEndereço IP Local: {Dns.GetHostAddresses(Dns.GetHostName()).Where(address => address.AddressFamily == AddressFamily.InterNetwork).First()} - Desktop: {Environment.MachineName}");
+                Console.ReadKey();
+            }
         }
-        while (username != "Emerson" || password != "!is@C#");
-            Console.Clear();
-		    Console.WriteLine("\nLogin efetuado com sucesso!");
-		    Console.WriteLine("\nAcessado em: " + dateTimeBrasilia.ToString("dd/MM/yyyy HH:mm:ss"));
-		    Console.ReadKey();
+        while (admin == false);
 	}
 }
