94a07b4db717bce7c097e3762b3ddbafc68729cf
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-03-08 19:58:53 -0300

    emerson-soares

--- Python/The Varieties/exercícios_complexidade_de_tempo.py
+++ Python/The Varieties/exercícios_complexidade_de_tempo.py
@@ -1,12 +1,12 @@
 # Exercicio 1: Ao final da execução da função abaixo,
 # Qual o número de execuções em função de n?
 '''
-1 + n + n² + n² + 1
-2n² + n + 2
+    1 + n + n² + n² + 1
+    2n² + n + 2
 '''
 # Qual a complexidade usando a notação O()?
 '''
-O(n²)
+    O(n²)
 '''
 def funcao1(n):
     s = 0                               # 1
@@ -18,12 +18,12 @@ def funcao1(n):
 # Exercicio 2: Ao final da execução da função abaixo,
 # Qual o número de execuções em função de n?
 '''
-1 + n + n² + n³ + n³ + 1
-2n³ + 2n² + 2
+    1 + n + n² + n³ + n³ + 1
+    2n³ + n² + n + 2
 '''
 # Qual a complexidade usando a notação O()?
 '''
-O(n³)
+    O(n³)
 '''
 def funcao2(n):
     s = 0                               # 1
@@ -36,25 +36,25 @@ def funcao2(n):
 # Exercicio 3: Considerando a função abaixo e responda as questões:
 # a) se a = 10 e b = 100, quantas vezes as linhas marcadas são executadas?
 '''
-a = 100
-b = 100
+    linha1 = a² = 10² 100
+    linha2 = b = 100
 '''
 # b) se a = 20 e b = 10, quantas vezes as linhas marcadas são executadas?
 '''
-a = 400
-b = 10
+    linha1 = a² = 20² = 400
+    linha2 = b = 10
 '''
 # c) se a = 100 e b = 10, quantas vezes as linhas marcadas são executadas?
 '''
-a = 1000
-b = 10
+    linha1 = a² = 100² = 10000
+    linha2 = b = 10
 '''
 # d) Qual a complexidade usando a notação O()?
 '''
-1 + n + n² + n² + n + n + 1
-2n² + 3n + 2
-
-O(n²)
+    1 + n + n² + n² + n + n + 1
+    2n² + 2n + n + 2
+    n²
+    O(n²)
 '''
 def funcao3(a, b):
     cont = 0                                        # 1
@@ -68,22 +68,22 @@ def funcao3(a, b):
 # Exercicio 4: Considerando a função abaixo e responda as questões:
 # a) se a lista tem tamanho 1000, quantas vezes as linhas marcadas são executadas?
 """
-1.000.000
+    1000²
 """
 # b) se a lista tem tamanho 2000, quantas vezes as linhas marcadas são executadas?
 """
-2.000.000
+    2000²
 """
 # c) se a lista tem tamanho 4000, quantas vezes as linhas marcadas são executadas?
 """
-4.000.000
+    4000²
 """
 # d) Qual a complexidade usando a notação O()?
 """
-1 + n + 2n²
-2n²
+    1 + n + 2n²
+    2n²
 
-O(n²)
+    O(n²)
 """
 
 def funcao4(lista):
@@ -96,23 +96,22 @@ def funcao4(lista):
 # Exercicio 5: Considerando a função abaixo e responda as questões:
 # a) se a lista tem tamanho 1000, quantas vezes as linhas marcadas são executadas?
 """
-1000
+    1000
 """
 # b) se a lista tem tamanho 2000, quantas vezes as linhas marcadas são executadas?
 """
-2000
+    2000
 """
 # c) se a lista tem tamanho 4000, quantas vezes as linhas marcadas são executadas?
 """
-4000
+    4000
 """
 # d) Qual a complexidade usando a notação O()?
 """
-1 + n + n
-1 + 2n
-2n
+    1 + 2n
+    2n
 
-O(n)
+    O(n)
 """
 
 def funcao5(lista):
@@ -123,25 +122,24 @@ def funcao5(lista):
 # Exercicio 6: Considerando a função abaixo e responda as questões:
 # a) se a lista tem tamanho 1000, quantas vezes as linhas marcadas são executadas?
 """
-linha 1 = 1000
-linha 2 = 1.000.000
+    linha 1 = 1000
+    linha 2 = n² = 1000²
 """
 # b) se a lista tem tamanho 2000, quantas vezes as linhas marcadas são executadas?
 """
-linha 1 = 2000
-linha 2 = 4.000.000
+    linha 1 = 2000
+    linha 2 = n² = 2000²
 """
 # c) se a lista tem tamanho 4000, quantas vezes as linhas marcadas são executadas?
 """
-linha 1 = 4000
-linha 2 = 16.000.000
+    linha 1 = 4000
+    linha 2 = n² = 4000²
 """
 # d) Qual a complexidade usando a notação O()?
 """
-1 + n + n + n + n² + n²
-2n² + 3n + 1
+    1 + 3n + 2n²
 
-O(n²)
+    O(n²)
 """
 
 def funcao6(lista):
@@ -157,14 +155,37 @@ def funcao6(lista):
 # Você não pode usar outro vetor como auxiliar e não deve utilizar funções prontas do python.
 # A função deve ter complexidade O(n).
 
-def funcao7(lista):
-    pass
+def troca(lista):
+    maior = lista[0] # 1
+    menor = lista[0] # 1
+    indice_maior = 0 # 1
+    indice_menor = 0 # 1
+    for i in range(len(lista)):                 # n
+        if lista[i] > maior:                    # n
+            maior = lista[i]                    # n
+            indice_maior = i                    # n
+        if lista[i] < menor:                    # n
+            menor = lista[i]                    # n 
+            indice_menor = i                    # n
+    lista[indice_maior] = menor                 # 1
+    lista[indice_menor] = maior                 # 1
 
+lista = [1, 3, 4, 90, 5]
+troca(lista)
+print(lista)
 
 # Exercicio 8:
 # Escreva uma função para inverter a ordem dos elementos de uma lista de tamanho n.
 # Você não pode usar outro vetor como auxiliar e não deve utilizar funções prontas do python.
 # A função deve ter complexidade O(n).
 
-def funcao8(lista):
-    pass
+def inverte(lista):
+    a = len(lista) - 1                  # a = n-1......... 0
+    for i in range(len(lista)//2):      # a = 0........... n-1
+        auxiliar = lista[i]
+        lista[i] = lista[a]
+        lista[a] = auxiliar
+        a -= 1
+lista = [1, 2, 3, 4, 5, 6]
+inverte(lista)
+print(lista)
